a3cbd709de002b5fb1c433afc506a6bf75da932c
rsgilbert <gssenyonjo@gmail.com>
2021-11-13 05:38:35 +0000

    Initial commit. Compiled hello.ts

---
+++ hello.ts
@@ -0,0 +1,10 @@
+const h = "Hi dear"
+
+console.log(h.toLowerCase())
+
+// This is an industrial-grade general-purpose greeter function
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date}`)
+}
+
+greet('Jonah')
