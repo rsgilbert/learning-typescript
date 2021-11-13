9a524899e399f3e971910796c5554061d04f55e4
rsgilbert <gssenyonjo@gmail.com>
2021-11-13 06:24:38 +0000

    looked at vrious strictness levels

---
+++ hello.js
@@ -0,0 +1,15 @@
+var h = "Hi dear";
+console.log(h.toLowerCase());
+// This is an industrial-grade general-purpose greeter function
+function greet(person, date) {
+    console.log("Hello " + person + ", today is " + date.toDateString());
+}
+greet('Jonah', new Date());
+// greet('Peter', 23)
+console.log(Date());
+console.log(new Date());
+function f12(age) {
+    console.log('Age is', age.ain);
+}
+var k = f12(3);
+console.log(k.toString());
--- hello.ts
+++ hello.ts
@@ -3,8 +3,18 @@ const h = "Hi dear"
 console.log(h.toLowerCase())
 
 // This is an industrial-grade general-purpose greeter function
-function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date}`)
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}`)
 }
 
-greet('Jonah')
+greet('Jonah', new Date())
+// greet('Peter', 23)
+console.log(Date())
+console.log(new Date())
+
+function f12(age) : any {
+    console.log('Age is', age.ain)
+}
+
+const k : null | string = f12(3)
+console.log(k.toString())
